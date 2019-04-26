029b8d7e110d2c3a3a67e32b6b1dfd9cee02e1b5
kharlbaron <inoue.sora@gmail.com>
2019-04-25 23:40:57 -0500

    AP-0 0. Created basic repository. 1. Created base scaffolding. 2. Added basic scripts. 3. Copied styles from another project (this is not about styles). 4. Ready for get fun.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './resources/images/logo.svg';
+import './resources/styles/App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="header-text">
+          Welcome Ornithorhynchus anatinus!
+        </h1>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
